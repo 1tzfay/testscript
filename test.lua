e0e2ed3a6e644b180b1ec6f6b979840a2571abd7
1tzfay <87763954+1tzfay@users.noreply.github.com>
2023-01-31 17:42:05 +0600

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,19 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Name", "RJTheme3")
+
+local Tab = Window:NewTab("Character")
+
+
+local Section = Tab:NewSection("WalkSpeed")
+
+Section:NewSlider("Speed", "(not recommended to go over 100)", 200, 20, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+local Section = Tab:NewSection("Jump")
+
+Section:NewSlider("JumpPower", "(not recommended to go over 150)", 500, 50, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
